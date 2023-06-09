b08089e2d3018ae02f7ca02fbd7bfcc38b15ad34
Alexy-vda <103523876+Alexy-vda@users.noreply.github.com>
2023-06-09 19:09:56 +0200

    add images components (#5)

---
+++ src/components/image/image.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import './image.css'
+
+export default function Illustration({ src, alt, type }) {
+    switch (type) {
+        case 'project':
+            return(
+                    <Image className='img' loading='lazy' height={240} width={384} src={src} alt={alt} />
+            )
+        break;
+        case 'content':
+            return(
+                    <Image className='img' loading='lazy' height={400} width={1216} src={src} alt={alt} />
+            )
+        break;
+        case 'default':
+            return(
+                    <Image className='img' loading='lazy' height={480} width={720} src={src} alt={alt} />
+            )
+        break;
+}
+}
---
+++ src/stories/image.stories.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Illustration from "../components/image/image";
+
+export default {
+    tags: ["autodocs"],
+    title: "Illustration",
+    component: Illustration,
+    argTypes: {
+        type: {
+            control: { type: "select" },
+            options:["project", "content","default"],
+        },
+    },
+};
+
+export const Project ={
+    args: {
+        src: "https://picsum.photos/240/384",
+        type: "project",
+        alt:"project",
+    },
+}
+export const Content ={
+    args: {
+        src: "https://picsum.photos/1216/400",
+        type: "content",
+        alt:"project",
+    },
+}
+export const Default ={
+    args: {
+        src: "https://picsum.photos/480/720",
+        type: "default",
+        alt:"project",
+    },
+}
