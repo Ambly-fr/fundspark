a2ab95df989b21eb9809ef85527dbb9e41ac7735
Alexy <alexy.vda@outlook.fr>
2023-06-09 19:52:26 +0200

    add tags component

---
+++ src/stories/tags.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tags from "../components/tags/tags";
+
+export default {
+    tags: ["autodocs"],
+    title: "Tags",
+    component: Tags,
+};
+
+export const Default ={
+    args: {
+        children: "Test",
+    },
+}
+
