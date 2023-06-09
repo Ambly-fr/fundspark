887699debb7aaa2f443f14738ac6689c5b80f35d
Alexy-vda <103523876+Alexy-vda@users.noreply.github.com>
2023-06-09 13:41:27 +0200

    add button (#3)

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,41 +1,39 @@
-import { Button } from './Button';
+import React from "react";
+import Button from "../components/button/button";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
-  component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
+    tags: ["autodocs"],
+    title: "Button",
+    component: Button,
+    argTypes: {
+        type: {
+            control: { type: "select" },
+            options: ["XL", "XS", "LinkXS", "LinkXL"],
+        },
     },
-  },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+export const xl ={
+    args: {
+        type: "XL",
+        label: "Primary",
+    },
+}
+export const xs ={
+    args: {
+        type: "XS",
+        label: "Primary",
+    },
+}
+export const linkXS ={
+    args: {
+        type: "LinkXS",
+        label: "Primary",
+    },
+}
+export const linkXL ={
+    args: {
+        type: "LinkXL",
+        label: "Primary",
+    },
+}
